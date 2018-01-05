1979a1908c548840e4829769f45da2810b82d556
DESKTOP-R3F6U42\Milica <milicamih88@gmail.com>
2018-01-06 00:07:23 +0100

    initial commit

---
+++ js/javascript.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('#click-acount').click(function() {
+            $('.ul2').toggle('1000');
+            $("i", this).toggleClass("fa-angle-up fa-angle-down"); 
+     });  
+
+
+ var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
+});
